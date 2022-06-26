36683b13d848a9bb19efa606c12f0d0e342cd951
bsymonica <bsymoncia@neowiz.com>
2022-06-26 21:27:42 +0900

    0626 home paintjs done

--- app.js
+++ app.js
@@ -4,16 +4,22 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange"); // 선 굵기 
 const mode = document.getElementById("jsMode"); // 버튼
+const saveBtn = document.getElementById("jsSave"); // save 버튼 기능 만들기
+
+const INITIAL_COLOR = "#2c2c2c"; // 디폴트 값
+const CANVAS_SIZE = 700;
 
 // canvas element는 2개의 사이즈를 가져야함(css와 이것) css로 캔버스를 그리고 있으나, 픽셀을 다루는 엘리먼트로 그리고 있으니까 엘리멘트에도 canvas의 사이즈를 지정해줘야 선을 그릴 수 있음
 // pixel modifier에 사이즈를 줌
-canvas.width = 700; 
-canvas.height = 700;
+canvas.width = CANVAS_SIZE; 
+canvas.height = CANVAS_SIZE;
 
-ctx.strokeStyle = "#2c2c2c"; // 선 색의 디폴트값
+ctx.fillStyle = "white"; //fill과 stroke을 검은색 디폴트로 하기 전에 캔버스 배경색 설정
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+
+ctx.strokeStyle = INITIAL_COLOR; // 선 색의 디폴트값
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = "2.5"; // 그리는 선의 굵기
-
-ctx.fillRect(50, 20, 100, 40); // 캔버스에 색상 박스 생성   
 
 let painting = false; // 클릭 이벤트에 대해서 페인팅 추가
 let filling = false;
@@ -42,6 +48,7 @@ function onMouseMove(event) { // 마우스 이벤트 주기
 function handleColorClick(event) {
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 }
 
 function handleRangeChange(event) {
@@ -56,6 +63,7 @@ function handleModeClick() {
     } else {
         filling = true;
         mode.innerText = "Paint"; // fill 동작 시 버튼 노출
+        ctx.fillStyle = ctx.strokeStyle; // 색채우기와 색상버튼의 색이 같도록
     }
 }
 
@@ -71,11 +79,34 @@ function handleModeClick() {
 }
 
 
+function handleCanvasClick() {
+    if (filling) {
+        ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE); // FILL 모드가 아닐 경우, 페인팅 되도록 
+    }
+}
+
+
+function handleCM(event) {
+    event.preventDefault(); // 마우스 오른쪽 이미지 저장 키이벤트 불가
+} 
+
+
+function handleSaveClick() {
+    const image = canvas.toDataURL(); // 이미지 저장 타입을 만듬, 기본 png
+    const link = document.createElement("a"); // 이미지 링크를 만듬
+    link.href = image; // href는 image(url)가 되어야 하고
+    link.download = "PaintJS[EXPORT]"; // 다운로드는 이름을 가지고 있어야 저장 네임이 설정됨
+    link.click();
+}
+
+
 if (canvas) { // 캔버스가 있는지 찾기
     canvas.addEventListener("mousemove", onMouseMove); // 마우스가 움직였을때 설정
     canvas.addEventListener("mousedown", startPainting); // 마우스 클릭했을때 발생하는 설정
     canvas.addEventListener("mouseup", stopPainting); //마우스 클릭 후 땠을때 발생하는 설정
     canvas.addEventListener("mouseleave", stopPainting); // 마우스가 캔버스 벗어나면 발생하는 설정, stopPainting을 직접 입력
+    canvas.addEventListener("click", handleCanvasClick); // 색채워넣기 fill
+    canvas.addEventListener("contextmenu", handleCM); // 마우스 오른쪽 금지
 }
 
 Array.from(colors).forEach(color => 
@@ -90,4 +121,8 @@ if (range) {
 
 if (mode) {
     mode.addEventListener("click", handleModeClick);
+}
+
+if (saveBtn) {
+    saveBtn.addEventListener("click", handleSaveClick);
 }
